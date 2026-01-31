3732e5dee1f1b704c8087d828e1170a87cc0131e
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-31 13:57:49 +0530

    Datatypes of JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all js code as newer version
+
+//we are using nodejs not browser so it will raise error in terminal
+//alert("ayushi")
+
+let name="ayushi"
+let age=18
+let isLoggedIn= false
+let temperature=null
+
+// primitive datatypes
+//number => 2 to power 53
+// BigInt
+// string = ""
+// boolean => true/false
+// null => standalone value (something assigned but empty)
+// undefined => theres no value assigned
+// symbol => unique
+
+console.log(typeof age)
+console.log(typeof "ayushi")
+console.log(typeof null)  // object - its not a defined type
+console.log(typeof undefined)  //undefined-its type is defined
