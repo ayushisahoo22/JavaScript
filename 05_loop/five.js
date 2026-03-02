a4c663e15f8f72da31a8e7717286f8f93c3afba8
ayushisahoo22 <ayushiis0912@gmail.com>
2026-03-02 20:57:09 +0530

    High order array loops

---
+++ 05_loop/five.js
@@ -0,0 +1,39 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)  //passing as reference and not as method
+
+// coding.forEach( (item, index, arr)=> {  //index and whole array can be accessed
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [   //array of objects
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
